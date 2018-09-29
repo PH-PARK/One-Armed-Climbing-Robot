fbf196975accc496a8b09a655ab7e289267d69fb
PH Park <35191440+PH-PARK@users.noreply.github.com>
2018-09-29 22:47:30 +0900

    update
    1. modifyed PIN numbers.
    2. added DIR gpio.
    3. added functions for all inputs.

--- main.c
+++ main.c
@@ -6,11 +6,10 @@
 #include <softPwm.h>
 #include <pthread.h>
 
-#define LEFT_PWM 11
-#define LEFT_DIR 12
-
-#define RIGHT_PWM 15
-#define RIGHT_DIR 16
+#define LEFT_PWM 0//physical num11
+#define LEFT_DIR 1//physical num12
+#define RIGHT_PWM 3//physical num15
+#define RIGHT_DIR 4//physical num16
 
 
 int main()
@@ -31,56 +30,76 @@ int main()
 	}
 
 	pinMode(LEFT_PWM, PWM_OUTPUT);
-	softPwmCreate(LEFT_PWM, 0, 100);
 	pinMode(RIGHT_PWM, PWM_OUTPUT);
+	softPwmCreate(LEFT_PWM, 0, 100);
+
+	pinMode(LEFT_DIR, OUTPUT);
+	pinMode(RIGHT_DIR, OUTPUT);
 	softPwmCreate(RIGHT_PWM, 0, 100);
 
 	for (;;)
-	{
-		c = serailGethar(s);
-		if (c == 97)// 'a'
+	{	
+		if (serialDataAvail(s) != -1)
 		{
-			putchar(c);
-			printf(": left\n");
-			fflush(stdout);
-			softPwmWrite(LEFT_PWM, 0);
-			softPwmWrite(RIGHT_PWM, 70);
-		}
-		else if (c == 100)//'d'
-		{
-			putchar(c);
-			printf(": right\n");
-			fflust(stdout);
-			softPwmWrite(LEFT_PWM, 70);
-			softPwmWrite(RIGHT_PWM, 0);
-		}
-		else if (c == 115)//'s'
-		{
-			putchar(c);
-			printf(": backword\n");
-			fflust(stdout);
-			softPwmWrite(LEFT_PWM, 0);
-			softPwmWrite(RIGHT_PWM, 0);
-		}
-		else if (c == 119)//'w'
-		{
-			putchar(c);
-			printf(": forward\n");
-			fflust(stdout);
-			softPwmWrite(LEFT_PWM, 70);
-			softPwmWrite(RIGHT_PWM, 70);
-		}
-		else if (c == 122)//'z'
-		{
-			putchar(c);
-			printf(": STOP\n");
-			fflush(stdout);
-			softPwmWrite(LEFT_PWM, 0);
-			softPwmWrite(RIGHT_PWM, 0);
-		}
-		else 
-		{
-			fflush(stdout);
-		}
-	}
-}
+			c = serailGethar(s);
+			if (c == 97)// 'a'
+			{
+				putchar(c); \
+				printf(": left\n");
+				fflush(stdout);
+				softPwmWrite(LEFT_PWM, 0);
+				digitalWrite(LEFT_DIR, HIGH);
+				softPwmWrite(RIGHT_PWM, 70);
+				digitalWrite(RIGHT_DIR, HIGH);
+			}
+			else if (c == 100)//'d'
+			{
+				putchar(c);
+				printf(": right\n");
+				fflust(stdout);
+				softPwmWrite(LEFT_PWM, 70);
+				digitalWrite(LEFT_DIR, LOW);
+				softPwmWrite(RIGHT_PWM, 0);
+				digitalWrite(RIGHT_DIR, LOW);
+			}
+			else if (c == 115)//'s'
+			{
+				putchar(c);
+				printf(": backword\n");
+				fflust(stdout);
+				softPwmWrite(LEFT_PWM, 0);
+				digitalWrite(LEFT_DIR, HIGH);
+				softPwmWrite(RIGHT_PWM, 0);
+				digitalWrite(RIGHT_DIR, LOW);
+			}
+			else if (c == 119)//'w'
+			{
+				putchar(c);
+				printf(": forward\n");
+				fflust(stdout);
+				softPwmWrite(LEFT_PWM, 70);
+				digitalWrite(LEFT_DIR, LOW);
+				softPwmWrite(RIGHT_PWM, 70);
+				digitalWrite(RIGHT_DIR, HIGH);
+			}
+			else if (c == 109)//'m'
+			{
+				putchar(c);
+				printf(": change mode\n");
+				fflush(stdout);
+			}
+			else if (c == 122)//'z'
+			{
+				putchar(c);
+				printf(": STOP\n");
+				fflush(stdout);
+				softPwmWrite(LEFT_PWM, 0);
+				softPwmWrite(RIGHT_PWM, 0);
+			}
+			else
+			{
+				fflush(stdout);
+			}//if(c==)
+		}//if(serialDataAvail(s)
+	}//for (;;)
+}//main
