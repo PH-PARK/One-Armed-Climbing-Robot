4ab1e5f759f041734a42720034ae855fe01df6c0
PH Park <35191440+PH-PARK@users.noreply.github.com>
2018-09-29 14:54:21 +0900

    PWM control by serial control
    add pinmode and pwmwrite

--- main.c
+++ main.c
@@ -6,6 +6,13 @@
 #include <softPwm.h>
 #include <pthread.h>
 
+#define LEFT_PWM 11
+#define LEFT_DIR 12
+
+#define RIGHT_PWM 15
+#define RIGHT_DIR 16
+
+
 int main()
 {
 	int s;
@@ -22,6 +29,12 @@ int main()
 		fprintf(stderr, "Unable to open serail device : %s\n", strerror(errno));
 		return 1;
 	}
+
+	pinMode(LEFT_PWM, PWM_OUTPUT);
+	softPwmCreate(LEFT_PWM, 0, 100);
+	pinMode(RIGHT_PWM, PWM_OUTPUT);
+	softPwmCreate(RIGHT_PWM, 0, 100);
+
 	for (;;)
 	{
 		c = serailGethar(s);
@@ -30,30 +43,40 @@ int main()
 			putchar(c);
 			printf(": left\n");
 			fflush(stdout);
+			softPwmWrite(LEFT_PWM, 0);
+			softPwmWrite(RIGHT_PWM, 70);
 		}
 		else if (c == 100)//'d'
 		{
 			putchar(c);
 			printf(": right\n");
 			fflust(stdout);
+			softPwmWrite(LEFT_PWM, 70);
+			softPwmWrite(RIGHT_PWM, 0);
 		}
 		else if (c == 115)//'s'
 		{
 			putchar(c);
 			printf(": backword\n");
 			fflust(stdout);
+			softPwmWrite(LEFT_PWM, 0);
+			softPwmWrite(RIGHT_PWM, 0);
 		}
 		else if (c == 119)//'w'
 		{
 			putchar(c);
 			printf(": forward\n");
 			fflust(stdout);
+			softPwmWrite(LEFT_PWM, 70);
+			softPwmWrite(RIGHT_PWM, 70);
 		}
 		else if (c == 122)//'z'
 		{
 			putchar(c);
 			printf(": STOP\n");
 			fflush(stdout);
+			softPwmWrite(LEFT_PWM, 0);
+			softPwmWrite(RIGHT_PWM, 0);
 		}
 		else 
 		{
