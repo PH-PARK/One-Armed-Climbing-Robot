3357ba432debc2edfb342ce0ddee52eddce255b9
PH Park <35191440+PH-PARK@users.noreply.github.com>
2018-10-02 19:39:51 +0900

    Dual mode applied.
    PC checks serial buffer after doing specific schedule. And if pc got character "m", it turns to remote control mode.

--- main.c
+++ main.c
@@ -18,6 +18,7 @@ int main()
 	int s;
 	int c;
 	uint8_t schedule;
+	uint8_t k;
 
 	if (wiringPiSetup() == -1)
 	{
@@ -46,93 +47,127 @@ int main()
 		{
 		case 0:
 			puts("schedule 0");
-			schedule++;
+			delay(100);
+			schedule=3;
+			k = 1;
 			break;
 		case 1:
 			puts("schedule 1");
-			schedule++;
+			schedule = 3;
+			k = 2;
 			break;
 		case 2:
 			puts("schedule 2");	
-			schedule=0;
+			schedule = 3;
+			k = 0;
 			break;
-		case 3: //
+		case 3:
+			puts("Checking buffer.");
+			if (serialDataAvail(s) == -1)
+			{
+				puts("Serial data error.");
+			}
+			else if (serialDataAvail(s) == 0)
+			{
+				puts("Biffer is Empty.");
+			}
+			else
+			{
+				c = serialGetchar(s);
+				putchar(c);
+				if (c == 109)
+				{
+					puts("Changing Mode.");
+					schedule = 4;
+					break;
+				}
+			}
+			schedule = k;
+			break;
+			
+		case 4: //
 			puts("Entering remote control mode.")
 			if (serialDataAvail(s) == -1)
 			{
 				puts("something wrong...");
-				break;
+				
 			}
-			if (serialDataAvail(s) == 0) 
+			else if (serialDataAvail(s) == 0) 
 			{
-				puts("there is no data can read.");
-				break;
-			}
-			c = serialGetchar(s);
-			if (c == 97)// 'a'
-			{
-				putchar(c); \
-					printf(": left\n");
-				fflush(stdout);
-				softPwmWrite(LEFT_PWM, 0);
-				digitalWrite(LEFT_DIR, HIGH);
-				softPwmWrite(RIGHT_PWM, 70);
-				digitalWrite(RIGHT_DIR, HIGH);
-			}
-			else if (c == 100)//'d'
-			{
-				putchar(c);
-				printf(": right\n");
-				fflust(stdout);
-				softPwmWrite(LEFT_PWM, 70);
-				digitalWrite(LEFT_DIR, LOW);
-				softPwmWrite(RIGHT_PWM, 0);
-				digitalWrite(RIGHT_DIR, LOW);
-			}
-			else if (c == 115)//'s'
-			{
-				putchar(c);
-				printf(": backword\n");
-				fflust(stdout);
-				softPwmWrite(LEFT_PWM, 30);
-				digitalWrite(LEFT_DIR, HIGH);
-				softPwmWrite(RIGHT_PWM, 30);
-				digitalWrite(RIGHT_DIR, LOW);
-			}
-			else if (c == 119)//'w'
-			{
-				putchar(c);
-				printf(": forward\n");
-				fflust(stdout);
-				softPwmWrite(LEFT_PWM, 70);
-				digitalWrite(LEFT_DIR, LOW);
-				softPwmWrite(RIGHT_PWM, 70);
-				digitalWrite(RIGHT_DIR, HIGH);
-			}
-			/*else if (c == 109)//'m' // should be included in self driving mode.
-			{
-				putchar(c);
-				printf(": change mode\n");
-				fflush(stdout);
-			}*/
-			else if (c == 122)//'z'
-			{
-				putchar(c);
-				printf(": STOP\n");
-				puts("Entering Self-Driving mode.")
-				fflush(stdout);
-				softPwmWrite(LEFT_PWM, 0);
-				digitalWrite(LEFT_DIR, LOW);
-				softPwmWrite(RIGHT_PWM, 0);
-				digitalWrite(RIGHT_DIR, HIGH);
-				schedule = 0;
-				break;
+				puts("waiting...");
+				delay(100);
 			}
 			else
 			{
-				puts("Got unidentified value.")
-				fflush(stdout);
-			}//if(c==)
+				c = serialGetchar(s);
+				if (c == 97)// 'a'
+				{
+					putchar(c); \
+						printf(": left\n");
+					fflush(stdout);
+					softPwmWrite(LEFT_PWM, 0);
+					digitalWrite(LEFT_DIR, HIGH);
+					softPwmWrite(RIGHT_PWM, 70);
+					digitalWrite(RIGHT_DIR, HIGH);
+				}
+				else if (c == 100)//'d'
+				{
+					putchar(c);
+					printf(": right\n");
+					fflust(stdout);
+					softPwmWrite(LEFT_PWM, 70);
+					digitalWrite(LEFT_DIR, LOW);
+					softPwmWrite(RIGHT_PWM, 0);
+					digitalWrite(RIGHT_DIR, LOW);
+				}
+				else if (c == 115)//'s'
+				{
+					putchar(c);
+					printf(": backword\n");
+					fflust(stdout);
+					softPwmWrite(LEFT_PWM, 30);
+					digitalWrite(LEFT_DIR, HIGH);
+					softPwmWrite(RIGHT_PWM, 30);
+					digitalWrite(RIGHT_DIR, LOW);
+				}
+				else if (c == 119)//'w'
+				{
+					putchar(c);
+					printf(": forward\n");
+					fflust(stdout);
+					softPwmWrite(LEFT_PWM, 70);
+					digitalWrite(LEFT_DIR, LOW);
+					softPwmWrite(RIGHT_PWM, 70);
+					digitalWrite(RIGHT_DIR, HIGH);
+				}
+				else if (c == 109)//'m' // should be included in self driving mode.
+				{
+					putchar(c);
+					printf(": change mode\n");
+					softPwmWrite(LEFT_PWM, 0);
+					softPwmWrite(RIGHT_PWM, 0);
+					schedule = 0;
+					fflush(stdout);
+				}
+				/*else if (c == 122)//'z'
+				{
+					putchar(c);
+					printf(": STOP\n");
+					puts("Entering Self-Driving mode.")
+					fflush(stdout);
+					softPwmWrite(LEFT_PWM, 0);
+					digitalWrite(LEFT_DIR, LOW);
+					softPwmWrite(RIGHT_PWM, 0);
+					digitalWrite(RIGHT_DIR, HIGH);
+					schedule = 0;
+					break;
+				}*/
+				else
+				{
+					puts("Got unidentified value.")
+					fflush(stdout);
+				}//if(c==)
+			}
 			break;
 		}//switch
 	}//for (;;)
